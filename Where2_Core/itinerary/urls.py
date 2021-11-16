26b0bccd432b4c1569e64da9f5bafab154f8a2f9
Macbeagle <79579427+Macbeagle@users.noreply.github.com>
2021-11-16 10:47:32 +1000

    Implement pages

--- Where2_Core/itinerary/urls.py
+++ Where2_Core/itinerary/urls.py
@@ -3,7 +3,7 @@ from itinerary import views
 from itinerary.views import HomeView, itinerary_current_view, itinerary_full_view, resturant_view, activity_view
 
 urlpatterns = [
-    path('', HomeView.as_view()),
+    path('', HomeView.as_view(), name="home"),
     path('itinerary_current/', views.itinerary_current_view, name='itinerary_current'),
     path('itinerary_full/', views.itinerary_full_view, name='itinerary_full'),
     path('resturants/', views.resturant_view, name='resturants'),
