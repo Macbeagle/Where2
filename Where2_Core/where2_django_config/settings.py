d586806cb16901f9ad09787cfb8b14c4745b6aa5
Macbeagle <79579427+Macbeagle@users.noreply.github.com>
2021-11-17 10:07:13 +1000

    new admin login and media framework

--- Where2_Core/where2_django_config/settings.py
+++ Where2_Core/where2_django_config/settings.py
@@ -120,7 +120,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+# Create media root for uploaded images
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
 
+MEDIA_URL= "/media/"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
